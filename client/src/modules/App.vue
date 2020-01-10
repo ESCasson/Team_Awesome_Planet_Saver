7a2b3923c7b861fec38946d002732947db3ad587
Charlie Anderson <charliepjanderson@gmail.com>
2020-01-10 21:07:00 +0000

    Adds css to module vues

--- client/src/modules/App.vue
+++ client/src/modules/App.vue
@@ -1,5 +1,15 @@
 <template lang="html">
   <div id="app">
+<div class="header">
+<h1>Planet Saver</h1>
+</div>
+<div class="navbar">
+<ul>
+<li>Home</li>
+<li>My Scores</li>
+<li>Help</li>
+</ul>
+</div>
   <learning-content />
   </div>
 </template>
@@ -15,4 +25,22 @@ export default {
 </script>
 
 <style lang="css" scoped>
+#app {
+font-family: Arial, sans-serif;
+}
+.header {
+display: flex;
+justify-content: center;
+background-color: skyblue;
+}
+
+.navbar ul{
+display: flex;
+list-style: none;
+background-color: #c0c0c0;
+margin-top: 0;
+padding-top: 0;
+justify-content: space-around;
+height: 30px;
+}
 </style>
