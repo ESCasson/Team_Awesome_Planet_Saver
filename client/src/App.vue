4dc2d3ecf1769719897ef3f39123d0d7c9c47147
johnhannahdev <john@johnhannahdev.com>
2020-01-11 15:06:50 +0000

    initial commit

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,23 @@
 <template lang="html">
 
+<h1>Hello John</h1>
+
+
 </template>
 
 <script>
 export default {
+
 }
 </script>
 
 <style lang="css" scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
