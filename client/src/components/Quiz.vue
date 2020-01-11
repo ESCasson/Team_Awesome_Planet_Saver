335e89eccb0b3bf9c3716edf190a8cea3ea46dec
Ellie <cassona08@gmail.com>
2020-01-11 16:30:38 +0000

    adds gets quiz question data from backend

--- client/src/components/Quiz.vue
+++ client/src/components/Quiz.vue
@@ -1,10 +1,37 @@
 <template lang="html">
-<h1>Quiz</h1>
+  <p>Quiz for module: {{quiz_required.module_id}}</p>
 </template>
 
 <script>
+import QuizsService from '../../helpers/QuizsService.js';
+
 export default {
   name: 'quiz',
+  props: ['module_id'],
+  data () {
+    return {
+      quizs: [],
+      quiz_required: null,
+    }
+  },
+
+  methods: {
+    fetchData(){
+      QuizsService.getQuizs()
+      .then(quizs => this.quizs = quizs)
+      .then(quizs => this.requiredQuiz())
+    },
+
+    requiredQuiz(){
+      let index = this.quizs.findIndex(quiz => quiz.module_id === this.module_id)
+      return this.quiz_required = this.quizs[index]
+    }
+  },
+
+  mounted() {
+    this.fetchData()
+
+  }
 }
 </script>
 
