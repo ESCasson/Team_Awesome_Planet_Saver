0a91d6d708d6ab9660b1dfed6ff6666d8bcb2c01
Charlie <19439935+charliepja@users.noreply.github.com>
2020-01-13 09:33:37 +0000

    Revert "Charlie/modules/list"

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-export const eventBus = new Vue()
-
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
