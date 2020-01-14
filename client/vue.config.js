1b4b7ab30a2f5b64c93b6b71709d17cf76c7ef8e
Ellie <cassona08@gmail.com>
2020-01-14 14:39:15 +0000

    updated css

--- client/vue.config.js
+++ client/vue.config.js
@@ -15,9 +15,6 @@ module.exports = {
     loaderOptions: {
       css: {
         // options here will be passed to css-loader
-      },
-      postcss: {
-        // options here will be passed to postcss-loader
       }
     }
   }
